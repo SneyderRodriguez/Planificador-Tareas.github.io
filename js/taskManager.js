9101bf1664f0f040a6cf4185155467c2b7eae9ae
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-28 16:51:35 -0500

    Consturtor taskManager actualizado

--- js/taskManager.js
+++ js/taskManager.js
@@ -4,15 +4,17 @@ class TaskManager {
         this.currentId = currentId;
     }
 
-    addTask(name, description, dueDate, status) {
+    addTask(name, category, priority, description, startDate, dueDate) {
         this.currentId++;
         this.tasks.push({
             id: this.currentId,
             name: name,
+            category:category,
+            priority:priority,
             description: description,
+            startDate:startDate,
             dueDate: dueDate,
             status: 'POR HACER'
         });
     }
 }
-const newTaskForm = document.querySelector('#task-form');
